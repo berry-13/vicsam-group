c217b3eddff88a2906a60d39647b96cf974016b6
Marco Beretta - Vicsam Group <m.beretta@vicsamgroup.it>
2025-07-01 17:46:49 +0200

    fix(ecosystem): ensure exec_mode is set correctly and add newline at end of file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'vicsam-group-api',
     script: 'server.js',
     instances: 1,
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
